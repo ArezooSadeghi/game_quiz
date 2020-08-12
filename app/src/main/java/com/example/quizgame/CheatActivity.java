48e968881c8621c00307101113eaca84d2a285cd
arezo <arezoo.sdghi@gmail.com>
2020-08-12 17:29:43 +0430

    CheatActivity

---
+++ app/src/main/java/com/example/quizgame/CheatActivity.java
@@ -0,0 +1,53 @@
+package com.example.quizgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.quizgame.Controller.QuizActivity;
+
+public class CheatActivity extends AppCompatActivity {
+    private Button mButtonYesCheat, mButtonNoCheat;
+
+    private TextView mTextViewAnswerOfQuestion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        Intent intent = getIntent();
+        intent.getBooleanExtra(QuizActivity.EXTRA_ANSWER_OF_QUESTION, false);
+
+        findViews();
+        setListeners();
+    }
+
+     public void findViews() {
+        mButtonYesCheat = findViewById(R.id.btn_yescheat);
+        mButtonNoCheat = findViewById(R.id.btn_nocheat);
+        mTextViewAnswerOfQuestion = findViewById(R.id.txt_answerofquestion);
+     }
+
+     public void setListeners() {
+        mButtonYesCheat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+            }
+        });
+
+        mButtonNoCheat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+            }
+        });
+     }
+
+}
